389207b7b0d6ccbc23f45596cdab2fbb9841de93
sandy <584667571@qq.com>
2018-03-03 22:28:34 +0800

    修改migration

--- db/migrate/20180303133128_create_accounts.rb
+++ db/migrate/20180303133128_create_accounts.rb
@@ -4,8 +4,8 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
       t.string   :name,                                   comment: "账户名"
       t.string   :card_number,                            comment: "账号"
       t.decimal  :initial_total, precision: 20, scale: 4, comment: "初始化金额"
-      t.integer  :status
-      t.integer  :operator
+      t.integer  :status,                                 comment: "状态"
+      t.integer  :operator,                               comment: "账户创建人ID"
       t.timestamps
     end
   end
