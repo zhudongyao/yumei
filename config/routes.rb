178171c27003fbfa6c504547bcf76aeeb90f6a02
raosheng <815011925@qq.com>
2018-03-03 18:34:36 +0800

    修改route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "admin/home#index"
 
+  root "admin/home#index"
 
   namespace :admin do
     root "home#index"
