760b5a66223e55055cd60c407b9738dfabd6e13f
raosheng <815011925@qq.com>
2018-03-03 22:22:04 +0800

    修改migration

--- db/migrate/20180303133236_create_finance_types.rb
+++ db/migrate/20180303133236_create_finance_types.rb
@@ -1,6 +1,10 @@
 class CreateFinanceTypes < ActiveRecord::Migration[5.0]
   def change
     create_table :finance_types do |t|
+      t.string :name
+      t.integer :kind
+      t.integer :usable
+      t.string :operator
 
       t.timestamps
     end
